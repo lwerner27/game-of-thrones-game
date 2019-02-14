c4c6dd8f6bd9d7bf3720416e06ffd05c6a24ab84
Lucas Werner <lwerner27@gmail.com>
2019-02-14 13:50:43 -0600

    added basic Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="grey darken-4">
+        <div class="nav-wrapper container">
+            <a href="#" class="brand-logo">Game of Thrones Game</a>
+            <!-- <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+            </ul> -->
+        </div>
+    </nav>
+</template>
