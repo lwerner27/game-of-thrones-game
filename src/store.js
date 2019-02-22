6bc33e40be1b50e887412170b9a699e352157ca6
Lucas Werner <lwerner27@gmail.com>
2019-02-22 10:44:12 -0600

    updated userPicks in the store

--- src/store.js
+++ src/store.js
@@ -9,133 +9,133 @@ export default new Vuex.Store({
     userId: null,
     leagueId: null,
     picksLocked: false,
-    userPicks: {
-      johnSnow: {
+    userPicks: [
+      {
         name: 'John Snow',
         alive: true,
         whiteWalker: false
       },
-      sansaStark: {
+      {
         name: "Sansa Stark",
         alive: true,
         whiteWalker: false
       },
-      aryaStark: {
+      {
         name: "Arya Stark",
         alive: true,
         whiteWalker: false
       },
-      branStark: {
+      {
         name: "Bran Stark",
         alive: true,
         whiteWalker: false
       },
-      cerseiLannister: {
+      {
         name: "Cersei Lannister",
         alive: true,
         whiteWalker: false
       },
-      jaimeLannister: {
+      {
         name: "Jaime Lannister",
         alive: true,
         whiteWalker: false
       },
-      tyrionLannister: {
+      {
         name: "Tyrion Lannister",
         alive: true,
         whiteWalker: false
       },
-      daenerysTargaryen: {
+      {
         name: "Daenerys Targaryen",
         alive: true,
         whiteWalker: false
       },
-      yaraGreyjoy: {
+      {
         name: "Yara Greyjoy",
         alive: true,
         whiteWalker: false
       },
-      melisandre: {
+      {
         name: "Melisandre",
         alive: true,
         whiteWalker: false
       },
-      jorahMormont: {
+      {
         name: "Jorah Mormont",
         alive: true,
         whiteWalker: false
       },
-      theHound: {
+      {
         name: "The Hound",
         alive: true,
         whiteWalker: false
       },
-      theMountain: {
+      {
         name: "The Mountain",
         alive: true,
         whiteWalker: false
       },
-      samwellTarley: {
+      {
         name: "Samwell Tarley",
         alive: true,
         whiteWalker: false
       },
-      gilly: {
+      {
         name: "Gilly",
         alive: true,
         whiteWalker: false
       },
-      lordVarys: {
+      {
         name: "Lord Varys",
         alive: true,
         whiteWalker: false
       },
-      brienne: {
+      {
         name: "Brienne",
         alive: true,
         whiteWalker: false
       },
-      davosSeaworth: {
+      {
         name: "Davos Seaworth",
         alive: true,
         whiteWalker: false
       },
-      bronn: {
+      {
         name: "Bronn",
         alive: true,
         whiteWalker: false
       },
-      podrickPayne: {
+      {
         name: "Podrick Payne",
         alive: true,
         whiteWalker: false
       },
-      tormundGiantsbane: {
+      {
         name: "Tormund Giantsbane",
         alive: true,
         whiteWalker: false
       },
-      greyWorm: {
+      {
         name: "Gray Worm",
         alive: true,
         whiteWalker: false
       },
-      gendry: {
+      {
         name: "Gendry",
         alive: true,
         whiteWalker: false
       },
-      bericDondarrion: {
+      {
         name: "Beric Dondarrion",
         alive: true,
         whiteWalker: false
       },
-      euronGrayjoy: {
+      {
         name: "Euron Grayjoy",
         alive: true,
         whiteWalker: false
       },
-    },
+    ],
     role: null,
 
   },
