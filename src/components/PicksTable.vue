692495aa79ba52c8405308fe4db82c90ecbd6272
Lucas Werner <lwerner27@gmail.com>
2019-02-22 11:32:07 -0600

    added basic table setup

---
+++ src/components/PicksTable.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="main-container">
+        <table>
+
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Alive</th>
+                    <th>White Walker</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr v-for="(pick, index) in userPicks" v-bind:key="index">
+                    <td>{{ pick.name }}</td>
+                    <td>
+                        <label>
+                            <input v-if="pick.alive === true" type="checkbox" class="filled-in" checked="checked" />
+                            <input v-else type="checkbox" class="filled-in" checked="unchecked" />
+                            <span></span>
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input v-if="pick.whiteWalker === true" type="checkbox" class="filled-in" checked="checked" />
+                            <input v-else type="checkbox" class="filled-in" checked="unchecked" />
+                            <span></span>
+                        </label>
+                    </td>
+                </tr>
+            </tbody>
+
+        </table>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    computed: {
+        ...mapState([
+            "userPicks"
+        ])
+    }
+}
+</script>
--- src/views/MakePicks.vue
+++ src/views/MakePicks.vue
@@ -6,10 +6,24 @@
                     <h3>Make Your Picks</h3>
                 </div>
             </div>
-            <div class="row"></div>
+            <div class="row">
+                <div class="col s12 m8 offset-m2">
+                    <picks-table></picks-table>
+                </div>
+            </div>
         </div>
     </div>
 </template>
+
+<script>
+import PicksTable from '../components/PicksTable'
+export default {
+    components: {
+        PicksTable
+    }
+}
+</script>
+
 
 <style scoped>
     .main-container {
