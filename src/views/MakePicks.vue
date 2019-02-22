1eabe77b0769f12a3756388042f1d2318b306d2f
Lucas Werner <lwerner27@gmail.com>
2019-02-22 10:39:28 -0600

    added the MakePicks view

---
+++ src/views/MakePicks.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main-container">
+        <div class="container">
+            <div class="row">
+                <div class="col s12 center">
+                    <h3>Make Your Picks</h3>
+                </div>
+            </div>
+            <div class="row"></div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .main-container {
+        margin-top: 64px;
+    }
+</style>
+
