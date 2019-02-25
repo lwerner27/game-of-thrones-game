e10b6de858ae193c5d12faa14cff145270f8ec2d
Lucas Werner <lwerner27@gmail.com>
2019-02-25 11:19:20 -0600

    added correct stylings to the save button

--- src/views/MakePicks.vue
+++ src/views/MakePicks.vue
@@ -11,6 +11,16 @@
                     <picks-table></picks-table>
                 </div>
             </div>
+            <div class="row">
+                <div class="col s12 m2 offset-m2">
+                    <button class="btn waves-effect waves-light black save-btn" type="submit" name="action">
+                        <div>
+                            Save
+                            <i class="material-icons left">save</i>
+                        </div>
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -27,5 +37,9 @@ export default {
 <style scoped>
     .table-container {
         margin-top: 64px;
+    }
+    .save-btn {
+        width: 100%;
+        text-align: left;
     }
 </style>
