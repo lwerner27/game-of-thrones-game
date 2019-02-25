86290b9e3e87a3d50a4e1339717743f891ed9c69
Lucas Werner <lwerner27@gmail.com>
2019-02-25 11:35:37 -0600

    removed the save button from this view

--- src/views/MakePicks.vue
+++ src/views/MakePicks.vue
@@ -6,21 +6,7 @@
                     <h3>Make Your Picks</h3>
                 </div>
             </div>
-            <div class="row">
-                <div class="col s12 m8 offset-m2">
-                    <picks-table></picks-table>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col s12 m2 offset-m2">
-                    <button class="btn waves-effect waves-light black save-btn" type="submit" name="action">
-                        <div>
-                            Save
-                            <i class="material-icons left">save</i>
-                        </div>
-                    </button>
-                </div>
-            </div>
+            <picks-table></picks-table>
         </div>
     </div>
 </template>
@@ -37,9 +23,5 @@ export default {
 <style scoped>
     .table-container {
         margin-top: 64px;
-    }
-    .save-btn {
-        width: 100%;
-        text-align: left;
     }
 </style>
