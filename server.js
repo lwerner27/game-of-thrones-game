c646feda813f488d3f148d699a6620657d244bfa
Lucas Werner <lwerner27@gmail.com>
2019-02-28 14:05:05 -0600

    added basic cors config

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express")
 const bodyParser = require('body-parser')
 const mongoose = require("mongoose")
 const routes = require('./routes')
+const cors = require("cors")
 
 const PORT = process.env.PORT || 5000
 const app = express()
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({
     extended: true
