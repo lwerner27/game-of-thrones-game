1dbd2ce3d49d8c29da9b18a2e12a1f1a1221b090
Lucas Werner <lwerner27@gmail.com>
2019-02-28 11:00:25 -0600

    added basic server boilerplate

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const bodyParser = require('body-parser')
+// const mongoose = require("mongoose")
+// const routes = require('./routes')
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+// app.use(routes)
+
+// // Connect to the Mongo DB
+// mongoose.connect(
+//     process.env.MONGODB_URI || 'mongodb://localhost:27017/TestAuth', {
+//         useNewUrlParser: true
+//     }
+// );
+
+
+app.listen(PORT, () => {
+    console.log("The server is listening on port " + PORT)
+})
