3e57f2517a87f651cacbff02d496924743855268
Lucas Werner <lwerner27@gmail.com>
2019-03-08 10:20:53 -0600

    removed default picks values from this component

--- src/components/PicksTable.vue
+++ src/components/PicksTable.vue
@@ -49,109 +49,7 @@ export default {
     ],
     data: function () {
         return {
-            picks: {
-                johnSnow:{
-                    name: "John Snow",
-                    status: "Alive"
-                },
-                sansaStark:{
-                    name: "Sansa Stark",
-                    status: "Alive"
-                },
-                aryaStark:{
-                    name: "Arya Stark",
-                    status: "Alive"
-                },
-                branStark:{
-                    name: "Bran Stark",
-                    status: "Alive"
-                },
-                cerseiLannister:{
-                    name: "Cersei Lannister",
-                    status: "Alive"
-                },
-                jaimeLannister:{
-                    name: "Jaime Lannister",
-                    status: "Alive"
-                },
-                tyrionLannister:{
-                    name: "Tyrion Lannister",
-                    status: "Alive"
-                },
-                daenerysTargaryen:{
-                    name: "Daenerys Targaryen",
-                    status: "Alive"
-                },
-                yaraGrayjoy:{
-                    name: "Yara Greyjoy",
-                    status: "Alive"
-                },
-                melisandre:{
-                    name: "Melisandre",
-                    status: "Alive"
-                },
-                jorahMormont:{
-                    name: "Jorah Mormont",
-                    status: "Alive"
-                },
-                theHound:{
-                    name: "The Hound",
-                    status: "Alive"
-                },
-                theMountain:{
-                    name: "The Mountain",
-                    status: "Alive"
-                },
-                samwellTarley:{
-                    name: "Samwell Tarley",
-                    status: "Alive"
-                },
-                gilly:{
-                    name: "Gilly",
-                    status: "Alive"
-                },
-                lordVarys:{
-                    name: "Lord Varys",
-                    status: "Alive"
-                },
-                brienne:{
-                    name: "Brienne",
-                    status: "Alive"
-                },
-                davosSeaworth:{
-                    name: "Davos Seaworth",
-                    status: "Alive"
-                },
-                bron:{
-                    name: "Bronn",
-                    status: "Alive"
-                },
-                podrickPayne:{
-                    name: "Podrick Payne",
-                    status: "Alive"
-                },
-                tormundGiantsbane:{
-                    name: "Tormund Giantsbane",
-                    status: "Alive"
-                },
-                grayWorm:{
-                    name: "Gray Worm",
-                    status: "Alive"
-                },
-                gendry:{
-                    name: "Gendry",
-                    status: "Alive"
-                },
-                bericDondarrion: {
-                    name: "Beric Dondarrion",
-                    status: "Alive"
-                },
-                euronGrayjoy:{
-                    name: "Euron Grayjoy",
-                    status: "Alive"
-                },
-                picksMade: true 
-            }
+            picks: {} // Will eventually hold the users picks obtained from the database.
         }
     },
     methods: {
@@ -171,9 +69,7 @@ export default {
         }
     },
     mounted() {
-        if (this.userPicks.picksMade) {
-            this.picks = this.userPicks
-        }
+        this.picks = this.userPicks
     }
 }
 </script>
