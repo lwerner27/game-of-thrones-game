01a50cce2917fd9792da3ce6854b12da59572546
Lucas Werner <lwerner27@gmail.com>
2019-03-08 14:30:02 -0600

    added link to the rules page and changed the title

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,12 @@
 <template>
     <nav class="grey darken-4">
         <div class="nav-wrapper container">
-            <a class="flow-text">Game of Game of Thrones</a>
+            <a class="flow-text">The Game of Game of Thrones</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <router-link tag="li" to="/rules">
+                    <a>Rules</a>
+                </router-link>
+            </ul>
         </div>
     </nav>
 </template>
