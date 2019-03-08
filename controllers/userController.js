2042a37b472719a3d987acf384ee16873042d286
Lucas Werner <lwerner27@gmail.com>
2019-03-08 11:35:16 -0600

    added a method for getting picks by userId

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,8 +5,19 @@ module.exports = {
     updatePicks: function(req, res) {
         db.User
         .findOneAndUpdate({ _id: req.body.userId }, { picks: req.body.picks })
-        .then(dbModel => res.json(dbModel))
+        .then(() => {
+            this.getUserPicks(req, res)
+        })
         .catch(err => res.status(422).json(err));
     },
+
+    getUserPicks: function(req, res) {
+        db.User
+        .findById(req.body.userId, "picks")
+        .then(dbModel => {
+            res.json(dbModel)
+        })
+        .catch(err => res.status(422).json(err))
+    }
     
 }
