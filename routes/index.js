a9e03aac7bc65d57a6d42d4532a732184b2e3c76
Lucas Werner <lwerner27@gmail.com>
2019-03-08 11:00:07 -0600

    added the api routes to the routes index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const authRoutes = require('./auth');
-// const apiRoutes = require("./api")
+const apiRoutes = require("./api")
 
 // API Routes
 router.use('/auth', authRoutes);
-// router.use('/api', apiRoutes)
+router.use('/api', apiRoutes)
 
 module.exports = router;
