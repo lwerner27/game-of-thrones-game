82370c732708929e2e630bbeb169372907aaf785
Lucas Werner <lwerner27@gmail.com>
2019-03-18 11:09:45 -0500

    Updated the basePicks object to include the bonus questions

--- Shared/basePicks.js
+++ Shared/basePicks.js
@@ -1,104 +1,111 @@
 const basePicks = {
-    johnSnow:{
-        name: "John Snow",
-        status: "Alive"
+    characterPicks: {
+        johnSnow: {
+            name: "John Snow",
+            status: "Alive"
+        },
+        sansaStark: {
+            name: "Sansa Stark",
+            status: "Alive"
+        },
+        aryaStark: {
+            name: "Arya Stark",
+            status: "Alive"
+        },
+        branStark: {
+            name: "Bran Stark",
+            status: "Alive"
+        },
+        cerseiLannister: {
+            name: "Cersei Lannister",
+            status: "Alive"
+        },
+        jaimeLannister: {
+            name: "Jaime Lannister",
+            status: "Alive"
+        },
+        tyrionLannister: {
+            name: "Tyrion Lannister",
+            status: "Alive"
+        },
+        daenerysTargaryen: {
+            name: "Daenerys Targaryen",
+            status: "Alive"
+        },
+        yaraGrayjoy: {
+            name: "Yara Greyjoy",
+            status: "Alive"
+        },
+        melisandre: {
+            name: "Melisandre",
+            status: "Alive"
+        },
+        jorahMormont: {
+            name: "Jorah Mormont",
+            status: "Alive"
+        },
+        theHound: {
+            name: "The Hound",
+            status: "Alive"
+        },
+        theMountain: {
+            name: "The Mountain",
+            status: "Alive"
+        },
+        samwellTarley: {
+            name: "Samwell Tarley",
+            status: "Alive"
+        },
+        gilly: {
+            name: "Gilly",
+            status: "Alive"
+        },
+        lordVarys: {
+            name: "Lord Varys",
+            status: "Alive"
+        },
+        brienne: {
+            name: "Brienne",
+            status: "Alive"
+        },
+        davosSeaworth: {
+            name: "Davos Seaworth",
+            status: "Alive"
+        },
+        bron: {
+            name: "Bronn",
+            status: "Alive"
+        },
+        podrickPayne: {
+            name: "Podrick Payne",
+            status: "Alive"
+        },
+        tormundGiantsbane: {
+            name: "Tormund Giantsbane",
+            status: "Alive"
+        },
+        grayWorm: {
+            name: "Gray Worm",
+            status: "Alive"
+        },
+        gendry: {
+            name: "Gendry",
+            status: "Alive"
+        },
+        bericDondarrion: {
+            name: "Beric Dondarrion",
+            status: "Alive"
+        },
+        euronGrayjoy: {
+            name: "Euron Grayjoy",
+            status: "Alive"
+        },
     },
-    sansaStark:{
-        name: "Sansa Stark",
-        status: "Alive"
-    },
-    aryaStark:{
-        name: "Arya Stark",
-        status: "Alive"
-    },
-    branStark:{
-        name: "Bran Stark",
-        status: "Alive"
-    },
-    cerseiLannister:{
-        name: "Cersei Lannister",
-        status: "Alive"
-    },
-    jaimeLannister:{
-        name: "Jaime Lannister",
-        status: "Alive"
-    },
-    tyrionLannister:{
-        name: "Tyrion Lannister",
-        status: "Alive"
-    },
-    daenerysTargaryen:{
-        name: "Daenerys Targaryen",
-        status: "Alive"
-    },
-    yaraGrayjoy:{
-        name: "Yara Greyjoy",
-        status: "Alive"
-    },
-    melisandre:{
-        name: "Melisandre",
-        status: "Alive"
-    },
-    jorahMormont:{
-        name: "Jorah Mormont",
-        status: "Alive"
-    },
-    theHound:{
-        name: "The Hound",
-        status: "Alive"
-    },
-    theMountain:{
-        name: "The Mountain",
-        status: "Alive"
-    },
-    samwellTarley:{
-        name: "Samwell Tarley",
-        status: "Alive"
-    },
-    gilly:{
-        name: "Gilly",
-        status: "Alive"
-    },
-    lordVarys:{
-        name: "Lord Varys",
-        status: "Alive"
-    },
-    brienne:{
-        name: "Brienne",
-        status: "Alive"
-    },
-    davosSeaworth:{
-        name: "Davos Seaworth",
-        status: "Alive"
-    },
-    bron:{
-        name: "Bronn",
-        status: "Alive"
-    },
-    podrickPayne:{
-        name: "Podrick Payne",
-        status: "Alive"
-    },
-    tormundGiantsbane:{
-        name: "Tormund Giantsbane",
-        status: "Alive"
-    },
-    grayWorm:{
-        name: "Gray Worm",
-        status: "Alive"
-    },
-    gendry:{
-        name: "Gendry",
-        status: "Alive"
-    },
-    bericDondarrion: {
-        name: "Beric Dondarrion",
-        status: "Alive"
-    },
-    euronGrayjoy:{
-        name: "Euron Grayjoy",
-        status: "Alive"
-    },
+    bonusQuestions: {
+        isDannyPrego: "",
+        nightKingKiller: "",
+        ironThroneSitter: ""
+    }
 }
 
 module.exports = basePicks
