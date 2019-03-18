c088fcdaa4b86e70ffec2f4331f7e8b3996e7740
Lucas Werner <lwerner27@gmail.com>
2019-03-18 15:06:48 -0500

    added the sidenav to the main app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <navbar />
+    <custom-side-nav></custom-side-nav>
     <router-view/>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar'
+import CustomSideNav from './components/CustomSideNav'
 export default {
   components: {
-    Navbar
+    Navbar,
+    CustomSideNav
   }
 }
 </script>
