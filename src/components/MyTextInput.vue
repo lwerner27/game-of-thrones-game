f8736f699d7bb6d3b36261efb446202331210a87
Lucas Werner <lwerner27@gmail.com>
2019-03-19 15:43:31 -0500

    now emits custom event on keyup.enter

--- src/components/MyTextInput.vue
+++ src/components/MyTextInput.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="input-field col s12">
-        <input :id="inputId" :type="inputType" v-bind:value="value" v-on:input="$emit('input', $event.target.value)">
+        <input :id="inputId" :type="inputType" v-bind:value="value" v-on:keyup.enter.stop.prevent="$emit('submit-login')" v-on:input="$emit('input', $event.target.value)">
         <label :for="labelFor"><slot></slot></label>
     </div>
 </template>
