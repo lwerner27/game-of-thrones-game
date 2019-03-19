a81b24e548216857707db24e2427e2f3dbef70c9
Lucas Werner <lwerner27@gmail.com>
2019-03-19 11:32:09 -0500

    added the history mode middleware

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express")
 const bodyParser = require('body-parser')
 const mongoose = require("mongoose")
 const routes = require('./routes')
-const cors = require("cors")
+// const cors = require("cors")
+var history = require('connect-history-api-fallback');
 
 const PORT = process.env.PORT || 5000
 const app = express()
 
-app.use(cors())
+// app.use(cors())
+app.use(history());
 
 app.use(express.static('dist'))
 
@@ -23,7 +25,6 @@ mongoose.connect(
         useNewUrlParser: true
     }
 );
-
 
 app.listen(PORT, () => {
     console.log("The server is listening on port " + PORT)
