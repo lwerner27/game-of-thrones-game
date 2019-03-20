ad40286c02901c9500cb8b46aa37947540cfa3c9
Lucas Werner <lwerner27@gmail.com>
2019-03-20 10:09:33 -0500

    added icon to the rules link

--- src/components/CustomSideNav.vue
+++ src/components/CustomSideNav.vue
@@ -1,5 +1,5 @@
 <template>
     <ul class="sidenav" id="mobile-demo">
-        <li><router-link to="/rules">RULES</router-link></li>
+        <li><router-link to="/rules"><i class="material-icons grey-text text-darken-4">format_list_numbered</i>RULES</router-link></li>
     </ul>
 </template>
