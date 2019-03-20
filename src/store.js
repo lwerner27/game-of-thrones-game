d7bdd4bff1662c32da4c9ac9310ac1db6edbb4f6
Lucas Werner <lwerner27@gmail.com>
2019-03-20 13:54:41 -0500

    username is now held in the store

--- src/store.js
+++ src/store.js
@@ -7,6 +7,7 @@ export default new Vuex.Store({
   state: {
     jwt: null,
     userId: null,
+    username: null,
     picksLocked: false,
     totalScore: 0,
     picks: null
@@ -15,6 +16,7 @@ export default new Vuex.Store({
     UPDATE_USER_INFO: function(state, userObj) {
       state.jwt = userObj.jwt
       state.userId = userObj.userId
+      state.username = userObj.username
       state.picksLocked = userObj.picksLocked
       state.totalScore = userObj.totalScore
       state.picks = userObj.picks
