3def4ef6f49181279bc6dee6e9bb188bf2c8028c
Lucas Werner <lwerner27@gmail.com>
2019-03-20 13:57:39 -0500

    basic setup for the user page

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,3 +1,43 @@
 <template>
-    <h1>This is the User page.</h1>
+    <div class="main-container">
+        <div class="container">
+            <div class="row">
+                <div class="col s12">
+                    <h1>lwerner</h1>
+                    <p>current score: 20</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+    data: function() {
+        return {
+            username: null,
+            totalScore: null,
+            picks: null
+        }
+    },
+    computed: {
+        ...mapState([
+            "jwt"
+        ])
+    },
+    mounted: function() {
+        if (this.$store.state.userId === this.$route.params.id) {
+            console.log("You are logged in as this user.")
+            let { username, picks, totalScore } = this.$store.state
+
+            this.username = username
+            this.totalScore = totalScore
+            this.picks = picks
+            
+        } else {
+            console.log("You are logged in a different user.")
+        }
+    }
+}
+</script>
