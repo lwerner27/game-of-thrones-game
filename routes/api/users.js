87627e478e20797b3f9be04246fd7a3fc3da7ba3
Lucas Werner <lwerner27@gmail.com>
2019-03-21 10:20:01 -0500

    fixed the getBasicUserData api route

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,13 +4,10 @@ const passport = require('passport');
 require('../../config/passport')(passport);
 const { getToken } = require("../../Shared")
 
-router.get("/test", passport.authenticate('jwt', { session: false }), (req, res) => {
+router.get("/user/:userId", passport.authenticate('jwt', { session: false }), (req, res) => {
     const token = getToken(req.headers);
     if (token) {
-        console.log("the test route is working.")
-        res.json({
-            msg: "the test route is working"
-        })
+        db.User.getBasicUserData(req, res)
     } else {
         return res.status(403).send({
             success: false,
