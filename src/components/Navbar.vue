a78988af1c9e4907c25950ac95dc7c4fbbf91941
Lucas Werner <lwerner27@gmail.com>
2019-03-21 14:35:42 -0500

    fixed icon order on navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,11 +6,15 @@
                 <a class="show-on-small hide-on-med-and-up brand-logo center">T.G.O.G.O.T.</a>
                 <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <router-link  v-if="userId" tag="li" to="/search"><a><i class="material-icons">search</i></a></router-link>
                     <router-link  v-if="userId" tag="li" :to="'/user/' + userId">
                         <a><i class="material-icons">account_circle</i></a>
                     </router-link>
-                    <router-link tag="li" to="/rules"><a>RULES</a></router-link>
+                    <router-link  v-if="userId" tag="li" to="/search">
+                        <a><i class="material-icons">search</i></a>
+                    </router-link>
+                    <router-link tag="li" to="/rules">
+                        <a>RULES</a>
+                    </router-link>
                 </ul>
             </div>
         </nav>
