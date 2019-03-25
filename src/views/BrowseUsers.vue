4770a2534f3435d696a1ba521b027920c4ffe29d
Lucas Werner <lwerner27@gmail.com>
2019-03-25 09:23:56 -0500

    changed page layout

--- src/views/BrowseUsers.vue
+++ src/views/BrowseUsers.vue
@@ -1,23 +1,27 @@
 <template>
     <div class="main-container">
         <div class="container">
-            <div class="row hr-row">
+            <div class="row">
                 <div class="col s12 m8 offset-m2">
-                    <h3>Browse Users</h3>
-                </div>
-            </div>
-            <div class="row hr-row">
-                <div class="col s12 m8 offset-m2">
-                    <hr>
-                </div>
-            </div>
-            <div class="row user-row" v-for="user in this.users" :key="user._id">
-                <div class="col s3 m1 offset-m2 center">
-                    <div class="btn grey darken-4">
-                        <i class="fas fa-user-plus"></i>
+                    <div class="row">
+                        <div class="col s12">
+                            <h2>Browse Users</h2>
+                            <hr>
+                        </div>
+                    </div>
+                    <div class="row user-row" v-for="user in this.users" :key="user._id">
+                        <div class="col s3 m2">
+                            <div class="btn grey darken-4">
+                                <i class="fas fa-user-plus"></i>
+                            </div>
+                        </div>
+                        <div class="col s9 m10">
+                            <router-link tag="a" :to="'/user/' + user._id">
+                                <h6>{{ user.username }}</h6>
+                            </router-link>
+                        </div>
                     </div>
                 </div>
-                <div class="col s9 m7"><router-link tag="a" :to="'/user/' + user._id"><h6>{{ user.username }}</h6></router-link></div>
             </div>
         </div>
     </div>
@@ -66,5 +70,3 @@ export default {
         background-color: #efefef
     }
 </style>
-
-
