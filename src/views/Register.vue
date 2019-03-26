308204eeefc44e3b7be394babcefe3b7605fceb5
Lucas Werner <lwerner27@gmail.com>
2019-03-26 14:33:17 -0500

    added the avatar selector and adjusted stylying

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,31 +1,50 @@
 <template>
     <div class="main-container">
         <div class="container">
+            
+            <div class="row top-row">
+                <div class="col s12">
+                    <h4>Select an Avatar</h4>
+                    <span>Scroll Right for more characters.</span>
+                </div>
+                <br>
+                <div class="col s12 characters-container">
+                    <img 
+                    class="character-img" 
+                    v-for="character in this.characters" 
+                    :key="character"
+                    :src="`../assets/CharacterImages/${character}.jpg`"
+                    :data-value="character"
+                    @click="changeAvatar"
+                >
+                </div>
+            </div>
+
             <div class="row">
-                <div class="col s12 m8 offset-m2 top-padding">
+                <div class="col s12 m2">
+                    <img :src="`../assets/CharacterImages/${this.selectedAvatar}.jpg`" alt="" class="responsive-img">
+                </div>
+                <div class="col s12 m8">
                     <custom-form>
 
-                        <div class="row input-row top-row">
+                        <div class="row input-row bottom-row">
                             <my-text-input v-model="username" inputType="text" inputId="username" labelFor="username">
                                 Username
                             </my-text-input>
-                        </div>
-
-                        <div class="row input-row">
-                            <my-text-input v-model="password" inputType="password" inputId="password" labelFor="password">
+                            <my-text-input v-model="password" inputType="password" inputId="password"
+                                labelFor="password">
                                 Password
                             </my-text-input>
-                        </div>
-                        
-                        <div class="row input-row">
-                            <my-text-input v-model="confirmPassword" inputType="password" inputId="confirmPassword" labelFor="confirmPassword">
+                            <my-text-input v-model="confirmPassword" inputType="password" inputId="confirmPassword"
+                                labelFor="confirmPassword">
                                 Confirm Password
                             </my-text-input>
                         </div>
 
-                        <div class="row input-row">
+                        <div class="row bottom-row input-row">
                             <div class="col s6">
-                                <a v-on:click="this.onSubmit" class="waves-effect waves-light btn grey darken-4">SUBMIT</a>
+                                <a v-on:click="this.onSubmit"
+                                    class="waves-effect waves-light btn grey darken-4">SUBMIT</a>
                             </div>
                         </div>
 
@@ -40,6 +59,8 @@
 import LoginForm from '../components/LoginForm'
 import MyTextInput from '../components/MyTextInput'
 import axios from 'axios'
+import characters from "../shared/characters.js"
+
 export default {
     components: {
         CustomForm: LoginForm,
@@ -49,7 +70,9 @@ export default {
         return {
             username: "",
             password: "",
-            confirmPassword:""
+            confirmPassword:"",
+            selectedAvatar: "Unknown",
+            characters: characters
         }
     },
     methods: {
@@ -68,25 +91,43 @@ export default {
             } else {
                 alert("Your passwords do not match.")
             }
+        },
+        changeAvatar: function(event) {
+            this.selectedAvatar = event.target.dataset.value
         }
-    }
+    },
 }
 </script>
 
 <style scoped>
-    .main-container {
-        height: 100vh
+    .characters-container {
+        margin-top: 5px;
+        display: flex;
+        flex-wrap: nowrap;
+        overflow-x: scroll;
     }
+    .container {
+        margin-top: 0px;
+    }
+
+    .character-img {
+        max-height: 175px;
+        margin: 5px;
+    }
+    
+    h4 {
+        margin: 0px;
+    }
+
     .input-row {
         margin-right: 10px;
         margin-left: 10px;
     }
-
-    .top-row {
-        margin-top: 20px;
+    .bottom-row {
+        margin-bottom: 0px;
     }
-
-    .top-padding {
-        padding-top: 80px;
+    .top-row {
+        margin-top: 0px;
+        padding-top: 10px;
     }
 </style>
