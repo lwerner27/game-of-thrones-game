939b442827873dfb6926e284bed3f6626a92ced5
Lucas Werner <lwerner27@gmail.com>
2019-03-28 16:23:24 -0500

    moved the view table to its own component

---
+++ src/components/ViewTable.vue
@@ -0,0 +1,52 @@
+<template>
+    <table class="striped">
+
+        <thead>
+            <tr>
+                <th>Name/Question</th>
+                <th>Selected Status/Answer</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr v-for="(pick, key) in picks.characterPicks" v-bind:key="key">
+                <td><strong>{{ pick.name }}</strong></td>
+                <td> {{ pick.status }} </td>
+            </tr>
+            <tr>
+                <td>
+                    <strong>Is Danaerys pregnant:</strong>
+                </td>
+                <td>
+                    {{ this.picks.bonusQuestions.isDannyPrego }}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <strong>Who kills the Night King:</strong>
+                </td>
+                <td>
+                    {{ this.picks.bonusQuestions.nightKingKiller }}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <strong>Who holds the iron throne:</strong>
+                </td>
+                <td>
+                    {{ this.picks.bonusQuestions.ironThroneSitter }}
+                </td>
+            </tr>
+        </tbody>
+
+    </table>
+</template>
+
+<script>
+export default {
+    props: [
+        "picks"
+    ]
+}
+</script>
+
